e1ba991c621436a32564228fb39d53159f25cf58
winegar1 <rl.win@icloud.com>
2017-10-20 15:00:58 -0500

    Sign inui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Rebecca on 10/20/17.
+//  Copyright © 2017 Rebecca. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
