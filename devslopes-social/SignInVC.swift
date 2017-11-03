8b3eaeeea828c42850285daf402193f6fd23d062
winegar1 <rl.win@icloud.com>
2017-11-03 14:27:29 -0500

    Completed creating Firebase DB users

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -42,24 +42,24 @@ class SignInVC: UIViewController {
 //                print("WINEGAR: Successfully authenticated with Facebook")
 //                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
 //                self.firebaseAuth(credential)
-//                self.completeSignIn(id: user.uid)
 //            }
 //        }
 //    }
-    
-        func firebaseAuth(_ credential: AuthCredential) {
-            Auth.auth().signIn(with: credential, completion: { (user, error) in
-                if error != nil {
-                    print("WINEGAR: Unable to authenticate with Firebase - \(String(describing: error))")
-                } else {
-                    print("WINEGAR: Successfully authenticated with Firebase")
-                    if let user = user {
-                        self.completeSignIn(id: user.uid)
-                    }
-                }
-                
-            })
-        }
+//
+//        func firebaseAuth(_ credential: AuthCredential) {
+//            Auth.auth().signIn(with: credential, completion: { (user, error) in
+//                if error != nil {
+//                    print("WINEGAR: Unable to authenticate with Firebase - \(String(describing: error))")
+//                } else {
+//                    print("WINEGAR: Successfully authenticated with Firebase")
+//                    if let user = user {
+//                        let userData = ["provider": user.credential.provider]
+//                        self.completeSignIn(id: user.uid, userData: userData)
+//                    }
+//                }
+//
+//            })
+//        }
     }
     @IBAction func signInTapped(_ sender: Any) {
         
@@ -68,7 +68,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("WINEGAR: Email user authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -77,7 +78,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("WINEGAR: Successfully authenticated with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -86,7 +88,8 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("WINEGAR: Data saved to keychain \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
